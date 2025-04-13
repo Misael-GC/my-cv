25b14d5af251324ac69a321a64c717404197502c
Misael Gomez Cuautle <misael.gomez1623@gmail.com>
2025-04-12 21:48:19 -0600

    Update tecnologies
    nuevas tecnologías

--- src/Pages/WhatDo/index.jsx
+++ src/Pages/WhatDo/index.jsx
@@ -13,7 +13,7 @@ function WhatDo() {
           <div className='flex flex-col items-start justify-center pl-4 w-auto'>
             <h2 className='text-slate-300 font-bold text-3xl mb-4'>Tecnologías</h2>
             <p className='flex flex-wrap px-10 max-w-[900px] py-7 mx-auto text-slate-300 font-semibold text-lg'>
-            En mi viaje por el mundo del desarrollo de software, he adquirido habilidades en una variedad de tecnologías frontend como HTML, CSS, y ReactJS, Tailwind y backend como Django y SQL. Además,
+            En mi viaje por el mundo del desarrollo de software, he adquirido habilidades en una variedad de tecnologías frontend como HTML, CSS, y ReactJS, Tailwind y backend como Django, PHP, Laravel, MySQL, SQL. Además,
             continúo aprendiendo nuevas herramientas para mantenerme actualizado en el campo.
                 
             </p>
